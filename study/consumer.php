d8fd6be61ed0b0bee2774d8518d0cc47cc0d5482
root <root@localhost.localdomain>
2021-02-11 17:02:56 +0800

    aa

--- study/consumer.php
+++ study/consumer.php
@@ -1,8 +1,9 @@
 <?php
 //过程：连接rmq  设置通道  设置交换机  设置队列
-//交换机  路由键
+//声明路由键 交换机的名称
 $exName = 'exchange_key';
 $routingKey = 'routing_key';
+$queueName = 'queuename';
 $config = [
     'host' => '192.168.146.130',
     'vhost' => '/',
@@ -13,8 +14,11 @@ $config = [
 
 //连接rmq
 $conn = new AMQPConnection($config);
+$conn->connect();
 //新建通道
 $channel = new AMQPChannel($conn);
+
+
 //交换机设置
 $ex = new AMQPExchange($channel);
 //设置交换机名称
@@ -30,20 +34,18 @@ $ex->setFlags(AMQP_DURABLE);
 
 //队列设置
 $q = new AMQPQueue($channel);
-$q->setName('queue');
+$q->setName($queueName);
 $q->setFlags(AMQP_DURABLE);
 $q->declareQueue();
 //队列在中间要绑定交换机与路由键
-$q->bind($ex->getName(), $routingKey);
+$q->bind($exName, $routingKey);
 // 接收消息并且进行处理的回调的方法
 $q->consume("receive");
 function receive($envelope, $queue)
 {
     //休眠两秒
     sleep(2);
-
-    echo $envelope->getBody() . '\n';
-
+    echo $envelope->getBody() . PHP_EOL;
     //显示确认  队列收到消息后 进行确认后  会删除消息
     $queue->ack($envelope->getDeliveryTag());
 }
